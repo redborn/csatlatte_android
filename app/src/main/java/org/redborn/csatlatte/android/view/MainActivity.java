61befe13983f3d0fd5bae8846567a6ae65868383
darkelfand <darkelfand@gmail.com>
2017-05-07 13:21:39 +0900

    chore : update config

---
+++ app/src/main/java/org/redborn/csatlatte/android/view/MainActivity.java
@@ -0,0 +1,15 @@
+package org.redborn.csatlatte.android.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.redborn.csatlatte.android.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
