479388580fb300608cea9ad50b1b54273bde40b6
choissgt <choissgt@gmail.com>
2017-06-14 15:00:35 +0900

    feat(view) : add random question result
    - 임의 문제 풀기 정답 및 해설 화면 추가

--- app/src/main/java/org/redborn/csatlatte/android/view/RandomQuestionResultActivity.java
+++ app/src/main/java/org/redborn/csatlatte/android/view/RandomQuestionResultActivity.java
@@ -1,16 +1,54 @@
 package org.redborn.csatlatte.android.view;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.design.widget.NavigationView;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import org.redborn.csatlatte.android.R;
+import org.redborn.csatlatte.android.view.commons.Navigation;
 
 /**
  * Created by admin on 2017-06-08.
  */
 
 public class RandomQuestionResultActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.random_question_result_activity);
+        AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.app_bar_layout);
+        appBarLayout.setBackgroundResource(R.drawable.random_question_test_title);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.random_question_result_drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        return false;
+        Intent intent = new Navigation().select(item, getApplicationContext());
+        startActivity(intent);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.random_question_result_drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 }
