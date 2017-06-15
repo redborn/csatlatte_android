58c020ee4b2c4f23f5da847d275416356a725aa5
choissgt <choissgt@gmail.com>
2017-06-15 17:42:58 +0900

    feat(view) : add login id
    - 아이디 입력 로그인 화면 추가

---
+++ app/src/main/java/org/redborn/csatlatte/android/view/LoginIdActivity.java
@@ -0,0 +1,80 @@
+package org.redborn.csatlatte.android.view;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import org.redborn.csatlatte.android.R;
+import org.redborn.csatlatte.android.view.commons.Navigation;
+
+/**
+ * Created by admin on 2017-06-15.
+ */
+
+public class LoginIdActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_id_activity);
+        ImageView appBarBackground = (ImageView) findViewById(R.id.app_bar_background);
+        appBarBackground.setImageResource(R.drawable.login_title);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.login_id_drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        View headerLayout = navigationView.getHeaderView(0);
+        LinearLayout linearLayout = (LinearLayout) headerLayout.findViewById(R.id.nav_header_layout);
+        linearLayout.setOnClickListener(this);
+
+        Button btnLoginNext = (Button) findViewById(R.id.btn_login_next);
+        btnLoginNext.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        Intent intent = new Navigation(getApplicationContext()).select(item);
+        startActivity(intent);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.login_id_drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        Context context = getApplicationContext();
+        Intent intent = new Navigation(context).header(view);
+
+        if (id == R.id.btn_login_next) {
+            intent = new Intent(context, MainActivity.class);
+        }
+
+        startActivity(intent);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.login_id_drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+    }
+}
