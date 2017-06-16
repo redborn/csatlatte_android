02cae25b4810136c67976e2de6d7e1438b474244
choissgt <choissgt@gmail.com>
2017-06-16 15:21:18 +0900

    feat(view) : add join login information
    - 회원가입 '수능라떼 로그인 정보' 페이지 추가

---
+++ app/src/main/java/org/redborn/csatlatte/android/view/JoinLoginInfoActivity.java
@@ -0,0 +1,64 @@
+package org.redborn.csatlatte.android.view;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import org.redborn.csatlatte.android.R;
+import org.redborn.csatlatte.android.view.commons.Navigation;
+
+/**
+ * Created by admin on 2017-06-16.
+ */
+
+public class JoinLoginInfoActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.join_login_info_activity);
+        ImageView appBarBackground = (ImageView) findViewById(R.id.app_bar_background);
+        appBarBackground.setImageResource(R.drawable.join_title);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Button btnJoinLoginInfoNext = (Button) findViewById(R.id.btn_join_login_info_next);
+        btnJoinLoginInfoNext.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        boolean result = super.onOptionsItemSelected(item);
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            onBackPressed();
+            result = true;
+        }
+
+        return result;
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        Context context = getApplicationContext();
+        Intent intent = new Navigation(context).header(view);
+
+        if (id == R.id.btn_join_login_info_next) {
+
+        }
+
+        if (intent != null) {
+            startActivity(intent);
+        }
+    }
+}
