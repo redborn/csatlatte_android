ec9c6738a1f9c336a096b80186cad01c42e4298a
choissgt <choissgt@gmail.com>
2017-06-16 15:41:10 +0900

    feat(view) : add Join security
    - 회원가입 보안 질문 페이지 추가

--- app/src/main/java/org/redborn/csatlatte/android/view/JoinSecurityActivity.java
+++ app/src/main/java/org/redborn/csatlatte/android/view/JoinSecurityActivity.java
@@ -7,22 +7,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Spinner;
 
 import org.redborn.csatlatte.android.R;
 import org.redborn.csatlatte.android.view.commons.Navigation;
+
+import java.util.ArrayList;
 
 /**
  * Created by admin on 2017-06-16.
  */
 
-public class JoinLoginInfoActivity extends AppCompatActivity implements View.OnClickListener {
+public class JoinSecurityActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.join_login_info_activity);
+        setContentView(R.layout.join_security_activity);
         ImageView appBarBackground = (ImageView) findViewById(R.id.app_bar_background);
         appBarBackground.setImageResource(R.drawable.join_title);
 
@@ -30,8 +34,18 @@ public class JoinLoginInfoActivity extends AppCompatActivity implements View.OnC
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        Button btnJoinLoginInfoNext = (Button) findViewById(R.id.btn_join_login_info_next);
-        btnJoinLoginInfoNext.setOnClickListener(this);
+        ArrayList<String> securityQuestionList = new ArrayList<String>();
+        securityQuestionList.add("꿈의 직업이 무엇인가요?");
+        securityQuestionList.add("어린 시절의 별명이 무엇이었나요?");
+        securityQuestionList.add("처음 배운 요리가 어떤건가요?");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, securityQuestionList);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner_security_question_list);
+        spinner.setAdapter(adapter);
+
+        Button btnJoinSecurityNext = (Button) findViewById(R.id.btn_join_security_next);
+        btnJoinSecurityNext.setOnClickListener(this);
     }
 
     @Override
@@ -53,8 +67,8 @@ public class JoinLoginInfoActivity extends AppCompatActivity implements View.OnC
         Context context = getApplicationContext();
         Intent intent = new Navigation(context).header(view);
 
-        if (id == R.id.btn_join_login_info_next) {
-            intent = new Intent(context, JoinSecurityActivity.class);
+        if (id == R.id.btn_join_security_next) {
+
         }
 
         if (intent != null) {
